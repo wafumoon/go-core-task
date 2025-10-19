0159e896487639883909970458060a4b4236f4d6
keizu <johnkillswitch9@gmail.com>
2025-10-20 00:36:12 +0300

    Код пятого задания. Добавлены необходимые тесты

---
+++ 5/main_5.go
@@ -0,0 +1,36 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+	a := []int{65, 3, 58, 678, 64}
+	b := []int{67, 2, 5, 43}
+
+	fmt.Println(CheckBoth(a, b))
+
+}
+
+func CheckBoth(slice1 []int, slice2 []int) (bool, []int) {
+
+	result := []int{}
+
+	newSlice := append([]int{}, slice1...)
+	newSlice = append(newSlice, slice2...)
+
+	for _, value := range newSlice {
+		if contains(slice1, value) && contains(slice2, value) && !contains(result, value) {
+			result = append(result, value)
+		}
+	}
+	return len(result) != 0, result
+
+}
+
+func contains(slice []int, target int) bool {
+	for _, item := range slice {
+		if item == target {
+			return true
+		}
+	}
+	return false
+}
