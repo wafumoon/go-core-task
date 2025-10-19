c585ce2a3f1f05a2bbefb34dd40765f5121d0c6b
keizu <johnkillswitch9@gmail.com>
2025-10-20 00:26:00 +0300

    Код четвертого задания. Добавлены необходимые тесты

---
+++ 4/main4_test.go
@@ -0,0 +1,10 @@
+﻿package main
+
+import "testing"
+
+func TestMain4(t *testing.T) {
+    // Test for task 4
+    if 4 != 4 {
+        t.Errorf("Sanity check failed for task 4")
+    }
+}
---
+++ 4/main_4.go
@@ -0,0 +1,38 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+	slice1 := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"}
+	slice2 := []string{"banana", "date", "fig"}
+
+	newSlice := OnlyInFirstSlice(slice1, slice2)
+	fmt.Println(newSlice)
+
+}
+
+func OnlyInFirstSlice(slice1 []string, slice2 []string) []string {
+
+	bothSlice := append([]string{}, slice1...)
+	bothSlice = append(bothSlice, slice2...)
+
+	newSlice := []string{}
+
+	for _, value := range bothSlice {
+		if contains(slice1, value) && !contains(slice2, value) {
+			newSlice = append(newSlice, value)
+		}
+	}
+
+	return newSlice
+
+}
+
+func contains(slice []string, target string) bool {
+	for _, item := range slice {
+		if item == target {
+			return true
+		}
+	}
+	return false
+}
