cb07330a7d80d82ca594c312f222d396441651d1
keizu <johnkillswitch9@gmail.com>
2025-10-22 22:02:12 +0300

    Код 9 задания. Добавлены необходимые тесты

---
+++ 9/main9_test.go
@@ -0,0 +1,10 @@
+﻿package main
+
+import "testing"
+
+func TestMain9(t *testing.T) {
+    // Test for task 9
+    if 9 != 9 {
+        t.Errorf("Sanity check failed for task 9")
+    }
+}
---
+++ 9/main_9.go
@@ -0,0 +1,49 @@
+﻿package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	numbers := []int{
+		15,
+		25,
+		44,
+		43,
+		15,
+		86,
+		11,
+		111,
+		1,
+		19,
+		50,
+	}
+
+	channelIn := make(chan int)
+	channelOut := make(chan float64)
+
+	go func() {
+		for _, number := range numbers {
+			channelIn <- number
+		}
+		close(channelIn)
+	}()
+
+	go func() {
+		for number := range channelIn {
+			channelOut <- math.Pow(float64(number), 3)
+		}
+		close(channelOut)
+	}()
+
+	// go func() {
+	// 	//number := math.Pow(float64(<-channelIn), 3)
+	// 	channelOut <- math.Pow(float64(<-channelIn), 3)
+
+	// }()
+
+	for result := range channelOut {
+		fmt.Println(result)
+	}
+}
